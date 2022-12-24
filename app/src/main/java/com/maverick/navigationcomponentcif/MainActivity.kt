5e121a68cab8f75c7db74d07f75e501b8190eb86
Maverick395006 <87902602+Maverick395006@users.noreply.github.com>
2022-12-24 17:57:44 +0530

    Option Menu Added

--- app/src/main/java/com/maverick/navigationcomponentcif/MainActivity.kt
+++ app/src/main/java/com/maverick/navigationcomponentcif/MainActivity.kt
@@ -2,9 +2,12 @@ package com.maverick.navigationcomponentcif
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.maverick.navigationcomponentcif.databinding.ActivityMainBinding
 
@@ -24,6 +27,15 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         setupActionBarWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
