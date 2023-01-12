841e83fad987ce2db7830ea24f80749fb7c7b2df
Maverick395006 <87902602+Maverick395006@users.noreply.github.com>
2023-01-12 13:32:57 +0530

    Blank Space Added

--- app/src/main/java/com/maverick/navigationcomponentcif/MainActivity.kt
+++ app/src/main/java/com/maverick/navigationcomponentcif/MainActivity.kt
@@ -12,6 +12,8 @@ import com.maverick.navigationcomponentcif.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
